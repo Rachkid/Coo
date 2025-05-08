ab11bbca4e3465e1cf4908285611ab3f6ec48a31
Rachkid <abouzeidirachid97@gmail.com>
2025-05-08 03:40:14 +0100

    Update check_file_exist.sh

--- check_file_exist.sh
+++ check_file_exist.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
-echo "Entrez le nom du fichier : " 
-read filename 
+read -p "Entrez le nom du fichier : " filename 
 if [ -f "$filename" ]; then
     echo "Le fichier '$filename' existe."
 else
